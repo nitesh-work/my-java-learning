232da8c90472f09cc9b14ea7e14641af414db95f
nitesh-work <159996965+nitesh-work@users.noreply.github.com>
2024-02-15 14:20:21 +0530

    Add files via upload

---
+++ ExampleOne.java
@@ -0,0 +1,14 @@
+public class ExampleOne {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        System.out.println(sum);
+        //int diff = b - a;
+        //System.out.println(diff);
+        //int mul = a * b;
+        //System.out.println(mul);
+        //double sub = b / a;
+        //System.out.println(sub);
+    }
+}

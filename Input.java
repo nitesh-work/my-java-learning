7256b68c33be89a9d80ea3aab44343ecfd1d2ff6
nitesh-work <159996965+nitesh-work@users.noreply.github.com>
2024-02-15 14:31:16 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        //nextInt
+        //nextFloat()
+        System.out.println(name);
+    }
+}

34f9c2d26fd4b304e0b0b945f7ffb5516ab3d917
nitesh-work <159996965+nitesh-work@users.noreply.github.com>
2024-02-15 14:36:29 +0530

    Add files via upload

---
+++ ExampleTwo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        //Take 2 variables 'a' & 'b' and print their sum.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}

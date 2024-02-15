956e1fd544f87454966f1fc44c8fa85a5c548c94
nitesh-work <159996965+nitesh-work@users.noreply.github.com>
2024-02-15 14:09:46 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Variables
+        String name = "tony stark";
+        int age = 48;
+        double price = 25.25;
+        int a = 25;
+        int b = 10;
+
+        b = 20;
+        name = "ironman";
+    }
+}

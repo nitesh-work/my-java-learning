745d3086c60d4927904b95e85e27cde8ffa62298
nitesh-work <159996965+nitesh-work@users.noreply.github.com>
2024-02-15 13:54:25 +0530

    Add files via upload

---
+++ OutPut.java
@@ -0,0 +1,8 @@
+public class OutPut {
+    public static void main(String[] args) {
+        //Output
+        System.out.println("Hello World");
+        System.out.println("Hello\nWorld");
+        System.out.print("Hello World");
+    }
+}

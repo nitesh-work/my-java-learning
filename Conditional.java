ab1bcfe48c0af4037a86e7b3b07684cd9284fada
nitesh-work <159996965+nitesh-work@users.noreply.github.com>
2024-02-15 19:01:29 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Conditional {
+    public static void main(String[] args) {
+        //if-else
+        Scanner sc = new Scanner(System.in);
+        /*
+        int age = sc.nextInt();
+
+        if (age > 18) {
+            System.out.println("Adult");;
+        } else {
+            System.out.println("Not Adult");
+        }
+        */
+    }
+}
